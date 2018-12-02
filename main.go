c3babc4024c3a3e321ecdb106aa562f24fba9a8b
Amir Raminfar <findamir@gmail.com>
2018-12-02 15:28:09 -0800

    Adds -m option for sending mms

--- main.go
+++ main.go
@@ -7,19 +7,21 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"path/filepath"
 	"strings"
 
 	flag "github.com/spf13/pflag"
 )
 
 var (
-	accountSid = ""
-	authToken  = ""
-	to         = ""
-	from       = ""
-	version    = "dev"
-	commit     = "none"
-	date       = "unknown"
+	accountSid      = ""
+	authToken       = ""
+	to              = ""
+	from            = ""
+	autoDetectMedia = false
+	version         = "dev"
+	commit          = "none"
+	date            = "unknown"
 )
 
 func init() {
@@ -27,10 +29,14 @@ func init() {
 	flag.StringVar(&authToken, "authToken", "", "Twilio authToken")
 	flag.StringVar(&to, "to", "", "To phone number")
 	flag.StringVar(&from, "from", "", "From phone number")
+	flag.BoolVarP(&autoDetectMedia, "detect-media", "m", false, "If enabled will send image URLs as MMS instead")
 	flag.Parse()
 }
 
 func main() {
+	log.SetFlags(0)
+	requireFlags("accountSid", "authToken", "to", "from")
+
 	urlStr := "https://api.twilio.com/2010-04-01/Accounts/" + accountSid + "/Messages.json"
 	client := &http.Client{}
 	scanner := bufio.NewScanner(os.Stdin)
@@ -39,7 +45,15 @@ func main() {
 		msgData := url.Values{}
 		msgData.Set("To", to)
 		msgData.Set("From", from)
-		msgData.Set("Body", scanner.Text())
+		text := scanner.Text()
+
+		parsed, err := url.Parse(text)
+		if autoDetectMedia && err == nil && isImage(parsed) {
+			msgData.Set("MediaUrl", parsed.String())
+		} else {
+			msgData.Set("Body", text)
+		}
+
 		msgDataReader := strings.NewReader(msgData.Encode())
 
 		req, _ := http.NewRequest("POST", urlStr, msgDataReader)
@@ -57,4 +71,20 @@ func main() {
 
 		}
 	}
+}
+
+func isImage(url *url.URL) bool {
+	ext := filepath.Ext(url.Path)
+	return url.IsAbs() &&
+		(ext == ".jpg" || ext == ".png" || ext == ".gif")
+}
+
+func requireFlags(names ...string) {
+	for _, name := range names {
+		f := flag.Lookup(name)
+		if !f.Changed {
+			log.Fatalf("Flag --%s is required", name)
+		}
+	}
+
 }
